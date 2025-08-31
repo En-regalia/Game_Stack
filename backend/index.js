dea7a1b1f185a55e85cd327c2adf5661fe1afb11
En-regalia <mike.ranner1@gmail.com>
2025-08-31 22:47:01 +0100

    inital work on server search functions with inital get requested started

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,22 @@
 const http = require('node:http');
 const express = require('express');
+const axios = require('axios')
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production') { // Server check to confirm dotenv file still required. move variables and defations to local valriable once in production. Change in dev .env file when relivent.
     require('dotenv').config();
 }
 
 const app = express();
 
 const PORT = process.env.PORT
+const searchRoute = process.env.SEARCH_URL
 
 app.listen(PORT,() => {
-    console.log(`Server running listening on port ${PORT}`);
+    console.log(`Server running listening on port ${PORT}`); // start up server message. Port defined in .env file in development.
 });
 
-
+app.get(searchRoute, async (rec, res) => {
+    const searchTerm = rec.query.s || "Scythe" // Setting the search query within the fuction to /searchTerm?s=search search term defined in .env file. Default value defined as "Scythe"
+})
 
 
