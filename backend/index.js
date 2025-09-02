66a2beb07ce45f0dd5b175379121e3ddf9a91c0d
En-regalia <mike.ranner1@gmail.com>
2025-09-02 22:38:22 +0100

    addition of Params for the axios.get currently hardcoded

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,15 @@ app.listen(PORT,() => {
 
 app.get(searchRoute, async (rec, res) => {
     const searchTerm = rec.query.s || "Scythe" // Setting the search query within the fuction to /searchTerm?s=search search term defined in .env file. Default value defined as "Scythe"
-    const response = await axios.get('https://boardgameprices.co.uk/api/search')
+    const response = await axios.get('https://boardgameprices.co.uk/api/search',{
+        Params: {
+            sitename: 'localhost3000',
+            search: searchTerm,
+            stock: 'Y',
+            currency: GBP
+
+        }
+    })
     
 
 
