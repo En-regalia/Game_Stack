76242fcdbb56cc3c21356f330e529c7eea6bbcf9
En-regalia <mike.ranner1@gmail.com>
2025-09-02 22:43:05 +0100

    update to comments

--- backend/index.js
+++ backend/index.js
@@ -16,11 +16,11 @@ app.listen(PORT,() => {
 });
 
 app.get(searchRoute, async (rec, res) => {
-    const searchTerm = rec.query.s || "Scythe" // Setting the search query within the fuction to /searchTerm?s=search search term defined in .env file. Default value defined as "Scythe"
+    const searchTerm = rec.query.s || "Scythe" // Setting the search query within the fuction to /search?s=search. Search URL (searchRoute) currenlty defined in .env. Default value defined as "Scythe"
     const response = await axios.get('https://boardgameprices.co.uk/api/search',{
-        Params: {
-            sitename: 'localhost3000',
+        Params: { // details of API paramiters that can be used. Currently hardcoded but once working will need to be updated to rec.query.xx variables to facilitate HTTP requests from front end as with searchTerm
             search: searchTerm,
+            sitename: 'localhost3000',
             stock: 'Y',
             currency: GBP
 
