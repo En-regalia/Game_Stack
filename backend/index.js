8a76f66196746ad85afa2d55389ec352df484f67
Michael Ranner (DHL Supply Chain) <michael.ranner@dhl.com>
2025-09-02 21:59:36 +0100

    update of get function with axios.get

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const http = require('node:http');
 const express = require('express');
 const axios = require('axios')
 
-if (process.env.NODE_ENV !== 'production') { // Server check to confirm dotenv file still required. move variables and defations to local valriable once in production. Change in dev .env file when relivent.
+if (process.env.NODE_ENV !== 'production') { // Server check to confirm dotenv file still required. Move variables and defations to local valriable once in production. Change in dev .env file when relivent.
     require('dotenv').config();
 }
 
@@ -12,11 +12,15 @@ const PORT = process.env.PORT
 const searchRoute = process.env.SEARCH_URL
 
 app.listen(PORT,() => {
-    console.log(`Server running listening on port ${PORT}`); // start up server message. Port defined in .env file in development.
+    console.log(`Server running listening on port ${PORT}`); // Start up server message. Port defined in .env file in development. 
 });
 
 app.get(searchRoute, async (rec, res) => {
     const searchTerm = rec.query.s || "Scythe" // Setting the search query within the fuction to /searchTerm?s=search search term defined in .env file. Default value defined as "Scythe"
+    const response = await axios.get('https://boardgameprices.co.uk/api/search')
+    
+
+
 })
 
 
