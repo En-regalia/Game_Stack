02f7f359916e3bcc90444e33555df66a76358106
En-regalia <mike.ranner1@gmail.com>
2025-09-04 21:00:33 +0100

    Finished params of external API search confirming search filed under query.s returns json object to frontend

--- backend/index.js
+++ backend/index.js
@@ -15,20 +15,18 @@ app.listen(PORT,() => {
     console.log(`Server running listening on port ${PORT}`); // Start up server message. Port defined in .env file in development. 
 });
 
-app.get(searchRoute, async (rec, res) => {
-    const searchTerm = rec.query.s || "Scythe" // Setting the search query within the fuction to /search?s=search. Search URL (searchRoute) currenlty defined in .env. Default value defined as "Scythe"
+app.get(searchRoute, async (req, res) => {
+    const searchTerm = req.query.s || "Scythe" // Setting the search query within the fuction to /search?s=search. Search URL (searchRoute) currenlty defined in .env. Default value defined as "Scythe"
     const response = await axios.get('https://boardgameprices.co.uk/api/search',{
-        Params: { // details of API paramiters that can be used. Currently hardcoded but once working will need to be updated to rec.query.xx variables to facilitate HTTP requests from front end as with searchTerm
+        params: { // details of API paramiters that can be used. Currently hardcoded but once working will need to be updated to rec.query.xx variables to facilitate HTTP requests from front end as with searchTerm
             search: searchTerm,
             sitename: 'localhost3000',
             stock: 'Y',
-            currency: GBP
+            currency: 'GBP',
 
         }
     })
-    
-
-
+    res.json(response.data) // returns json object data to the frontend
 })
 
 
