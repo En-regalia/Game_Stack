1f537e2aa238887180092669a644522bc9d07536
Michael Ranner (DHL Supply Chain) <michael.ranner@dhl.com>
2025-09-05 14:14:07 +0100

    Update of index.js layout for ease of reading before implimenting server files. Also updated database.js to export server object. Linking to index.js

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,25 @@
 const http = require('node:http');
 const express = require('express');
 const axios = require('axios')
+const db = require('.db/database.js')
+const searchRoutes = require('./routes/search.js')
+
+const app = express();
 
 if (process.env.NODE_ENV !== 'production') { // Server check to confirm dotenv file still required. Move variables and defations to local valriable once in production. Change in dev .env file when relivent.
     require('dotenv').config();
 }
 
-const app = express();
-
+//.env variables defined in .env file
 const PORT = process.env.PORT
-const searchRoute = process.env.SEARCH_URL
+const searchURL = process.env.SEARCH_URL
+
 
 app.listen(PORT,() => {
     console.log(`Server running listening on port ${PORT}`); // Start up server message. Port defined in .env file in development. 
 });
 
-
-// Search function using the external API "https://boardgameprices.co.uk/api/plugin"
-app.get(searchRoute, async (req, res) => {
-    const searchTerm = req.query.s || "Scythe" // Setting the search query within the fuction to /search?s=search. Search URL (searchRoute) currenlty defined in .env. Default value defined as "Scythe"
-    const response = await axios.get('https://boardgameprices.co.uk/api/search',{
-        params: { // details of API paramiters that can be used. Currently hardcoded but once working will need to be updated to rec.query.xx variables to facilitate HTTP requests from front end as with searchTerm
-            search: searchTerm,
-            sitename: 'localhost3000',
-            stock: 'Y',
-            currency: 'GBP',
-
-        }
-    })
-    res.json(response.data) // returns json object data to the frontend
-})
+// mounting differnt server functions.
+app.use(searchURL,searchRoutes) // search function
 
 
